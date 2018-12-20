4212f9c64b7bf96cceeacb5ecfedfed6834c5070
Tak H Kim <takhyun818@gmail.com>
2018-12-20 22:14:53 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,7 +6,7 @@
         @length_2 = length_2
         @length_3 = length_3
     end
-
+    def kind
     if length_1 <= 0 || length_2 <= 0 || length_3 <= 0
                 raise TriangleError
             elsif length_1 + length_2 <= length_3 || length_3 + length_1 <= length_2 || length_2 + length_3 <= length_1
