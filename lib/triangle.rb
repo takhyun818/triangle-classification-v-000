9db86963a30adf8f2669f32f8abbeec8d2cb0dea
Tak H Kim <takhyun818@gmail.com>
2018-12-20 22:13:37 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,4 +6,25 @@
         @length_2 = length_2
         @length_3 = length_3
     end
+    if length_1 <= 0 || length_2 <= 0 || length_3 <= 0
+                raise TriangleError
+            elsif length_1 + length_2 <= length_3 || length_3 + length_1 <= length_2 || length_2 + length_3 <= length_1
+                raise TriangleError
+            elsif length_1 == length_2 && length_2 == length_3 && length_1 == length_3
+                return :equilateral
+            elsif length_1 == length_2 || length_2 == length_3 || length_1 == length_3
+                return :isosceles
+            elsif length_1 != length_2 && length_2 != length_3 && length_1 != length_3
+                return :scalene
+            else
+                false
+            end
+        end
+
+    end
+
+    class TriangleError < StandardError
+        def message
+            "invalid triangle: please look up the definition of a triangle."
+        end
 end
