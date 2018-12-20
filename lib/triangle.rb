852ad4d7399dbe3550bae4b5cacd7c32729952bb
Tak H Kim <takhyun818@gmail.com>
2018-12-20 22:14:00 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,6 +6,7 @@
         @length_2 = length_2
         @length_3 = length_3
     end
+
     if length_1 <= 0 || length_2 <= 0 || length_3 <= 0
                 raise TriangleError
             elsif length_1 + length_2 <= length_3 || length_3 + length_1 <= length_2 || length_2 + length_3 <= length_1
@@ -23,8 +24,8 @@
 
     end
 
-    class TriangleError < StandardError
-        def message
-            "invalid triangle: please look up the definition of a triangle."
-        end
+class TriangleError < StandardError
+  def message
+    "invalid triangle: please look up the definition of a triangle."
+  end
 end
